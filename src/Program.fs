c41575373e12931dfb83cb537fee84079a2e2b77
Dave Falkner <ardave@gmail.com>
2018-03-31 11:06:12 -0700

    improvements

--- src/Program.fs
+++ src/Program.fs
@@ -1,22 +1,19 @@
 ﻿open System
-open System.Collections.Generic
 open System.IO
 open System.Security.Cryptography
 
-
-let dupesFile = "dupes.txt"
+type Hash     = Hash     of string
+type FileName = FileName of string
+let dupesFile  = "dupes.txt"
+let errorsFile = "errors.txt"
 let logToDupesFile str = File.AppendAllLines(dupesFile, [|str|])
 let md5 = MD5.Create()
-let sizes = Dictionary<Int64, FileInfo>()
-let hashes = Dictionary<string, FileInfo>()
 let mutable bytes = 0L
 let mutable fileCount = 0
 
-type Hash     = Hash     of string
-type FileName = FileName of string
-
 let getLength (fi:FileInfo) = fi.Length
 let getFullName (fi:FileInfo) = FileName fi.FullName
+let unpackFileName (FileName str) = str
 
 let split resultSequence =
     let oList, eList =
@@ -39,9 +36,10 @@ let getHash (fileInfo:FileInfo) =
 
 let tryGetHash fileInfo =
     try
-        getHash fileInfo |> Ok
+        let hash = getHash fileInfo
+        Ok(hash, fileInfo)
     with
-        | :? IOException as ex -> Error ex
+        | :? IOException as ex -> Error(ex, fileInfo)
 
 let inline stringf format (x : ^a) = 
     (^a : (member ToString : string -> string) (x, format))
@@ -50,19 +48,20 @@ let printProgress fileCount bytes =
     let megs = bytes / 1_000_000L
     printfn "Files processed: %i, megabytes processed: %s MB" fileCount (megs |> stringf "N0")
 
+let printStatus1 x =
+    printfn "Found %i unique file sizes." (x |> Seq.length)
+    x
+
+let printStatus2 x =
+    printfn "Found %i unique file sizes matching more than 1 file" (x |> Seq.length)
+    x
+
 [<EntryPoint>]
 let main _ =
     File.Delete dupesFile
     let allFiles = Directory.GetFiles("/Volumes/4gig", "*.*", SearchOption.AllDirectories)
 
     printfn "Found %i files." allFiles.Length
-    let printStatus1 x =
-        printfn "Found %i unique file sizes." (x |> Seq.length)
-        x
-
-    let printStatus2 x =
-        printfn "Found %i unique file sizes matching more 1 file" (x |> Seq.length)
-        x
 
     allFiles
     |> Seq.map FileInfo
@@ -72,13 +71,13 @@ let main _ =
     |> printStatus2
     |> Seq.sortByDescending fst
     |> Seq.iter(fun (_, fis) ->
-        let hashTuples = fis |> Seq.map(fun fi -> fi |> tryGetHash, fi)
-        
-        let okays, errors = hashTuples |> Seq.map fst |> split |> Seq.zip (hashTuples |> Seq.map snd)
+        let hashTuples = fis |> Seq.map tryGetHash
+
+        let okays, errors = hashTuples |> split 
 
 
         let dupes =
-            hashTuples
+            okays
             |> Seq.groupBy fst
             |> Seq.filter(fun gp -> gp |> snd |> Seq.length > 1)
 
@@ -91,11 +90,12 @@ let main _ =
                     |> snd
                     |> getLength
                     |> stringf "N0"
-                logToDupesFile (sprintf "The following files all have size %s and hash %s" size (gp |> fst))
+                let (Hash hashstr) = fst gp
+                logToDupesFile (sprintf "The following files all have size %s and hash %s" size hashstr)
 
                 gp
                 |> snd
-                |> Seq.iter (snd >> getFullName >> logToDupesFile)
+                |> Seq.iter (snd >> getFullName >> unpackFileName >> logToDupesFile)
                 logToDupesFile ""
             )
         )
