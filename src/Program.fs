c9174cedda08270c41f4640f2f68996f20745241
Dave Falkner <ardave@gmail.com>
2018-03-31 11:10:44 -0700

    Refactored to cut down on potential redundant hashing of some files.

--- src/Program.fs
+++ src/Program.fs
@@ -1,19 +1,30 @@
 ﻿open System
-open System.Collections.Generic
 open System.IO
 open System.Security.Cryptography
 
-
-let dupesFile = "dupes.txt"
+type Hash     = Hash     of string
+type FileName = FileName of string
+let dupesFile  = "dupes.txt"
+let errorsFile = "errors.txt"
 let logToDupesFile str = File.AppendAllLines(dupesFile, [|str|])
 let md5 = MD5.Create()
-let sizes = Dictionary<Int64, FileInfo>()
-let hashes = Dictionary<string, FileInfo>()
 let mutable bytes = 0L
 let mutable fileCount = 0
 
 let getLength (fi:FileInfo) = fi.Length
-let getFullName (fi:FileInfo) = fi.FullName
+let getFullName (fi:FileInfo) = FileName fi.FullName
+let unpackFileName (FileName str) = str
+
+let split resultSequence =
+    let oList, eList =
+        resultSequence
+        |> Seq.fold(fun (os, es) elem ->
+            match elem with
+            | Ok o -> o::os, es
+            | Error e -> os, e::es
+        ) ([],[])
+    let prepare lst = lst |> List.rev |> Seq.ofList
+    prepare oList, prepare eList
 
 let getHash (fileInfo:FileInfo) =
     let toStr hashArray =  BitConverter.ToString(hashArray).Replace("-", "").ToLowerInvariant()
@@ -21,18 +32,14 @@ let getHash (fileInfo:FileInfo) =
     File.OpenRead(fileInfo.FullName)
     |> md5.ComputeHash
     |> toStr
+    |> Hash
 
 let tryGetHash fileInfo =
     try
-        getHash fileInfo |> Ok
+        let hash = getHash fileInfo
+        Ok(hash, fileInfo)
     with
-        | :? IOException as ex -> Error ex
-let sizeMatch (fi1:FileInfo) (fi2:FileInfo) =
-    let hash1 = fi1 |> getHash
-    let hash2 = fi2 |> getHash
-    if hash1 = hash2 then
-        let message = [|sprintf "Files \n\t%s\n\t%s\nare the same, with hashes %s and %s" fi1.FullName fi2.FullName hash1 hash2|]
-        File.AppendAllLines(dupesFile, message)
+        | :? IOException as ex -> Error(ex, fileInfo)
 
 let inline stringf format (x : ^a) = 
     (^a : (member ToString : string -> string) (x, format))
@@ -41,19 +48,20 @@ let printProgress fileCount bytes =
     let megs = bytes / 1_000_000L
     printfn "Files processed: %i, megabytes processed: %s MB" fileCount (megs |> stringf "N0")
 
+let printStatus1 x =
+    printfn "Found %i unique file sizes." (x |> Seq.length)
+    x
+
+let printStatus2 x =
+    printfn "Found %i unique file sizes matching more than 1 file" (x |> Seq.length)
+    x
+
 [<EntryPoint>]
 let main _ =
     File.Delete dupesFile
     let allFiles = Directory.GetFiles("/Volumes/4gig", "*.*", SearchOption.AllDirectories)
 
     printfn "Found %i files." allFiles.Length
-    let printStatus1 x =
-        printfn "Found %i unique file sizes." (x |> Seq.length)
-        x
-
-    let printStatus2 x =
-        printfn "Found %i unique file sizes matching more 1 file" (x |> Seq.length)
-        x
 
     allFiles
     |> Seq.map FileInfo
@@ -63,9 +71,13 @@ let main _ =
     |> printStatus2
     |> Seq.sortByDescending fst
     |> Seq.iter(fun (_, fis) ->
-        let hasheTuples = fis |> Seq.map(fun fi -> fi |> tryGetHash, fi)
+        let hashTuples = fis |> Seq.map tryGetHash
+
+        let okays, errors = hashTuples |> split 
+
+
         let dupes =
-            hasheTuples
+            okays
             |> Seq.groupBy fst
             |> Seq.filter(fun gp -> gp |> snd |> Seq.length > 1)
 
@@ -78,11 +90,12 @@ let main _ =
                     |> snd
                     |> getLength
                     |> stringf "N0"
-                logToDupesFile (sprintf "The following files all have size %s and hash %s" size (gp |> fst))
+                let (Hash hashstr) = fst gp
+                logToDupesFile (sprintf "The following files all have size %s and hash %s" size hashstr)
 
                 gp
                 |> snd
-                |> Seq.iter (snd >> getFullName >> logToDupesFile)
+                |> Seq.iter (snd >> getFullName >> unpackFileName >> logToDupesFile)
                 logToDupesFile ""
             )
         )
