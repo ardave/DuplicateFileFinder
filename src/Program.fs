0b213a8195b4dadc32a1ac001a965e7da5e84a8c
Dave Falkner <ardave@gmail.com>
2018-04-02 22:52:04 -0700

    readability improvements

--- src/Program.fs
+++ src/Program.fs
@@ -44,9 +44,11 @@ let tryGetHash fileInfo =
 let inline stringf format (x : ^a) = 
     (^a : (member ToString : string -> string) (x, format))
 
+let formatWithCommas (x:int64) = stringf "N0" x
+
 let printProgress fileCount bytes =
     let megs = bytes / 1_000_000L
-    printfn "Files processed: %i, megabytes processed: %s MB" fileCount (megs |> stringf "N0")
+    printfn "Files processed: %i, megabytes processed: %s MB" fileCount (megs |> formatWithCommas)
 
 let printStatus1 x =
     printfn "Found %i unique file sizes." (x |> Seq.length)
@@ -56,40 +58,46 @@ let printStatus2 x =
     printfn "Found %i unique file sizes matching more than 1 file" (x |> Seq.length)
     x
 
+let getGroupingsGreaterThan1 gp = gp |> snd |> Seq.length > 1
+
 [<EntryPoint>]
 let main _ =
     File.Delete dupesFile
     let allFiles = Directory.GetFiles("/Volumes/4gig", "*.*", SearchOption.AllDirectories)
 
     printfn "Found %i files." allFiles.Length
+    let groupLength = fst
 
     allFiles
     |> Seq.map FileInfo
     |> Seq.groupBy(fun fi -> fi.Length)
     |> printStatus1
-    |> Seq.filter(fun gp -> gp |> snd |> Seq.length > 1)
+    |> Seq.filter getGroupingsGreaterThan1
     |> printStatus2
-    |> Seq.sortByDescending fst
+    |> Seq.sortByDescending groupLength
     |> Seq.iter(fun (_, fis) ->
         let hashTuples = fis |> Seq.map tryGetHash
-
         let okays, errors = hashTuples |> split 
-
-
+        let hash = fst
         let dupes =
             okays
-            |> Seq.groupBy fst
-            |> Seq.filter(fun gp -> gp |> snd |> Seq.length > 1)
+            |> Seq.groupBy hash
+            |> Seq.filter getGroupingsGreaterThan1
 
+        let keepHashFileInfoTuple = snd
+        let keepFileInfo = snd
+        let formatWithCommas (x:int64) = stringf "N0" x
+        
         dupes
-        |> Seq.iter(fun gp ->
+        |> Seq.iter(fun (gp:Hash * seq<Hash * FileInfo>) ->
                 let size =
                     gp
-                    |> snd
+                    |> keepHashFileInfoTuple
                     |> Seq.head
-                    |> snd
+                    |> keepFileInfo
                     |> getLength
-                    |> stringf "N0"
+                    |> formatWithCommas
+
                 let (Hash hashstr) = fst gp
                 logToDupesFile (sprintf "The following files all have size %s and hash %s" size hashstr)
 
