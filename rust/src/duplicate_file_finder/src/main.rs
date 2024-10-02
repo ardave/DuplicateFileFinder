fef7266098911c6e7bef0d3699cd2415bc2ce21a
David Falkner <ardave@gmail.com>
2024-10-02 16:13:44 -0600

    lots of progress

--- rust/src/duplicate_file_finder/src/main.rs
+++ rust/src/duplicate_file_finder/src/main.rs
@@ -1,6 +1,7 @@
 use std::{collections::HashMap, env, fs::{File, OpenOptions}, io::{self, BufWriter, Read, Seek, SeekFrom, Write}, path::PathBuf};
 use anyhow::Result;
 use md5::Digest;
+use num_format::{Locale, ToFormattedString};
 use walkdir::WalkDir;
 
 fn main()-> Result<()> {
@@ -8,16 +9,96 @@ fn main()-> Result<()> {
     println!("Args looks like: {:?}", args);
     match args.as_slice() {
         [current_path ] => search_path_for_dupes(&current_path)?,
-        | [_current, explicitly_requested] => search_path_for_dupes(explicitly_requested)?,
+        [_current, explicitly_requested] => search_path_for_dupes(explicitly_requested)?,
         _ => println!("You must include the folder path to scan.")
     }
 
     Ok(())
 }
 
-fn search_path_for_dupes(path: &str) -> Result<()> {
-    let mut by_size: HashMap<u64, Vec<(u64, PathBuf)>> = HashMap::new();
+fn get_file_hashes(flattened: Vec<(u64, PathBuf)>) -> Result<HashMap<(Digest, u64), Vec<PathBuf>>> {
+    let mut by_hash: HashMap<(Digest, u64), Vec<PathBuf>> = HashMap::new();
 
+    for (file_size, path_buf) in flattened {
+        match load_first_and_last_1024_bytes(&path_buf) {
+            Ok(bytes) => {
+                let digest = md5::compute(bytes);
+                if let Some(path_bufs) = by_hash.get_mut(&(digest, file_size)) {
+                    path_bufs.push(path_buf);
+                } else {
+                    by_hash.insert((digest,file_size), vec![path_buf]);
+                }
+                let hashed_count = by_hash.values().map(|v| v.len()).sum::<usize>();
+                if hashed_count % 100 == 0 {
+                    println!("{} files hashed.", hashed_count.format_number());
+                }
+            },
+            Err(e) => println!("Hash file bytes error: {}", e),
+        }
+    }
+
+    Ok(by_hash)
+}
+
+fn search_path_for_dupes(path: &str) -> Result<()> {
+    let mut by_size = get_files_in_path_grouped_by_size(path)?;
+
+    println!("Examined {} files.", by_size.values().map(|v| v.len()).sum::<usize>().format_number());
+    by_size.retain(|_, value| value.len() > 1);
+    println!("Duplicates by size: {}", by_size.values().map(|v| v.len()).sum::<usize>().format_number());
+
+    let flattened: Vec<(u64, PathBuf)> = by_size
+        .into_values()
+        .flat_map(|v| v)
+        .collect();
+
+    let mut by_hash = get_file_hashes(flattened)?;
+
+    by_hash.retain(|_, value| value.len() > 1);
+    
+    // Convert HashMap into a Vec of key-value pairs
+    let mut by_hash: Vec<_> = by_hash.into_iter().collect();
+
+    // Sort the Vec by key/file_size in descending order
+    by_hash.sort_by(|a, b| b.0.1.cmp(&a.0.1));
+
+    print_output_to_file(by_hash)  
+
+}
+
+fn print_output_to_file(by_hash: Vec<((Digest, u64), Vec<PathBuf>)>) -> Result<()> {
+    let file = OpenOptions::new()
+        .write(true)       // Open for writing
+        .create(true)      // Create the file if it doesn't exist
+        .truncate(true)    // Overwrite the file if it exists
+        .open("dupes.txt")?;
+
+    let mut writer = BufWriter::new(file);
+
+    //println!("Examined {} files.", by_size.values().map(|v| v.len()).sum::<usize>().format_number());
+    let total_dupe_bytes: u64 = by_hash
+        .iter()
+        .map(|((_, file_size), path_bufs)| file_size * path_bufs.len() as u64)
+        .sum();
+
+    let total_dupe_bytes = total_dupe_bytes.format_number();
+
+    println!("Dupes found totaling {} bytes:", total_dupe_bytes);
+    writeln!(writer, "Dupes found totaling {} bytes:", total_dupe_bytes)?;
+    for ((_digest, file_size), path_bufs) in by_hash {
+        println!("Duplicates of size {}", file_size.format_number());
+        writeln!(writer, "Duplicates of size {}", file_size.format_number())?;
+        for path_buf in path_bufs {
+            println!("\t{:?}", path_buf);
+            writeln!(writer, "\t{:?}", path_buf)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn get_files_in_path_grouped_by_size(path: &str) -> Result<HashMap<u64, Vec<(u64, PathBuf)>>> {
+    let mut by_size: HashMap<u64, Vec<(u64, PathBuf)>> = HashMap::new();
     for entry in WalkDir::new(path) {
         match entry {
             Ok(dir_entry) => {
@@ -29,82 +110,18 @@ fn search_path_for_dupes(path: &str) -> Result<()> {
                             path_bufs.push((file_size, path_buf));
                         } else {
                             by_size.insert(file_size, vec![(file_size, path_buf)]);
-                        }                    
-                    }                
+                        }
+                    }
                 }
             },
             Err(e) => println!("WalkDir Error: {}", e)
         }
     }
-
-    println!("Examined {} files.", by_size.values().map(|v| v.len()).sum::<usize>());
-    by_size.retain(|_, value| value.len() > 1);
-    println!("Duplicates by size: {}", by_size.values().map(|v| v.len()).sum::<usize>());
-
-    let flattened: Vec<(u64, PathBuf)> = by_size
-        .into_values()
-        .flat_map(|v| v)
-        .collect();
-
-    let mut by_hash: HashMap<(Digest, u64), Vec<PathBuf>> = HashMap::new();
-    for (file_size, path_buf) in flattened {
-        let mut file = File::open(&path_buf)?;
-        let mut buffer = vec![0; 1024];  // Initialize with a capacity of 1024 bytes
-        let read_result = file.read(&mut buffer);
-
-        match read_result {
-            Ok(bytes_read) => {
-                buffer.truncate(bytes_read);
-                let digest = md5::compute(buffer);
-                if let Some(path_bufs) = by_hash.get_mut(&(digest, file_size)) {
-                    path_bufs.push(path_buf);
-                } else {
-                    by_hash.insert((digest,file_size), vec![path_buf]);
-                }
-                let hashed_count = by_hash.values().map(|v| v.len()).sum::<usize>();
-                if hashed_count % 100 == 0 {
-                    println!("{} files hashed.", hashed_count);
-                }
-
-            },
-            Err(e) => println!("Hash file bytes error: {}", e),
-        }
-    }
-
-    by_hash.retain(|_, value| value.len() > 1);
-    
-    // Convert HashMap into a Vec of key-value pairs
-    let mut by_hash: Vec<_> = by_hash.into_iter().collect();
-
-    // Sort the Vec by key in descending order
-    by_hash.sort_by(|a, b| b.0.1.cmp(&a.0.1));
-
-
-        // Open the file in write mode, overwriting it if it exists
-    let file = OpenOptions::new()
-        .write(true)       // Open for writing
-        .create(true)      // Create the file if it doesn't exist
-        .truncate(true)    // Overwrite the file if it exists
-        .open("dupes.txt")?;
-
-    let mut writer = BufWriter::new(file);
-
-    println!("Dupes found:");
-    writeln!(writer, "Dupes found:")?;
-    for ((_digest, file_size), path_bufs) in by_hash {
-        println!("Duplicates of size {}", file_size);
-        writeln!(writer, "Duplicates of size {}", file_size)?;
-        for path_buf in path_bufs {
-            println!("\t{:?}", path_buf);
-            writeln!(writer, "\t{:?}", path_buf)?;
-        }
-    }
-
-    Ok(())
+    Ok(by_size)
 }
 
-fn load_first_and_last_1024_bytes(filename: &str) -> io::Result<Vec<u8>> {
-    let mut file = File::open(filename)?;
+fn load_first_and_last_1024_bytes(path_buf: &PathBuf) -> io::Result<Vec<u8>> {
+    let mut file = File::open(path_buf)?;
 
     // Get file size
     let file_size = file.metadata()?.len();
@@ -136,3 +153,22 @@ fn load_first_and_last_1024_bytes(filename: &str) -> io::Result<Vec<u8>> {
         Ok(first_1024[..first_bytes_read].to_vec())
     }
 }
+
+
+
+trait FormatNumber {
+    fn format_number(&self) -> String;
+}
+
+impl FormatNumber for usize {
+    fn format_number(&self) -> String {
+        self.to_formatted_string(&Locale::en)
+    }
+}
+
+impl FormatNumber for u64 {
+    fn format_number(&self) -> String {
+        self.to_formatted_string(&Locale::en)
+    }
+}
+
